dda316a3401d5c5d1415a0b49f1061fc32e2e2c9
dsazykin <daniel.sazykin@gmail.com>
2026-06-23 17:30:26 +0200

    feat(core): added conflict resolve window

---
+++ GitLoom.App/Views/ConflictResolverWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GitLoom.App.Views;
+
+public partial class ConflictResolverWindow : Window
+{
+    public ConflictResolverWindow()
+    {
+        InitializeComponent();
+    }
+}
